63ccb93975a4dab5004271acd665f8803bdcdcea
mmeder2016 <mmeder2016@gmail.com>
2017-02-22 11:19:28 -0600

    Beginning the directory architecture.

--- server.js
+++ server.js
@@ -1,23 +1,47 @@
-// Dependencies
+// DEPENDENCIES
 var express = require("express");
 var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
-// Snatches HTML from URLs
 var request = require('request');
-// Scrapes our HTML
 var cheerio = require('cheerio');
-
 var mongoose = require('mongoose');
 
-// Initialize Express
+// EXPRESS SETUP
 var app = express();
 
+// HANDLEBARS SETUP
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
+// METHOD OVERRIDE SETUP
+// var methodOverride = require('method-override');
+// app.use(methodOverride('_method'));
 
-// This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-    console.log("Database Error:", error);
+// BODY PARSER SETUP
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// ALLOW CORS http://enable-cors.org/server_expressjs.html
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
 });
+
+// SET PORT PLATFORM INDEPENDENT 
+app.set('port', (process.env.PORT || 3000));
+
+// DATABASE MODELS
+var db = require('./models');
+
+// SET ROUTES
+var router = require('./router');
+router(app, db, __dirname);
+
 
 
 // Set the app to listen on port 3000
