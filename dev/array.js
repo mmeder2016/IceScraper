bb60b8d006333ac69d0a38f73c76c39406b73c6a
mmeder2016 <mmeder2016@gmail.com>
2017-02-24 20:31:04 -0600

    Now displays comments.

---
+++ dev/array.js
@@ -0,0 +1,94 @@
+﻿var StoryArray = [{
+        _id: "1",
+        headline: "AAA",
+        link: "AAALLL",
+        __v: 0,
+        comments: [
+            "11",
+            "22",
+            "33"
+        ]
+    },
+    {
+        _id: "2",
+        headline: "BBB",
+        link: "BBBLLL",
+        __v: 0,
+        comments: [
+            "11",
+            "22"
+        ]
+    },
+    {
+        _id: "3",
+        headline: "CCC",
+        link: "CCCLLL",
+        __v: 0,
+        comments: ["11"]
+    }
+];
+
+var CommentArray = [{
+        _id: "11",
+        body: "ZZZ",
+        __v: 0
+    },
+    {
+        _id: "22",
+        body: "YYY",
+        __v: 0
+    },
+    {
+        _id: "33",
+        body: "XXX",
+        __v: 0
+    }
+];
+
+
+// var sObj = JSON.stringify(StoryArray);
+
+// for (var i = 0; i < CommentArray.length; i++) {
+//     var cObj = JSON.stringify(CommentArray[i]);
+
+//     sObj.replace(CommentArray[i]._id, cObj);
+// }
+
+// console.log(sObj);
+
+
+var resObj = [];
+
+for (var i = 0; i < StoryArray.length; i++) {
+    var s = {
+        _id: StoryArray[i]._id,
+        headline: StoryArray[i].headline,
+        link: StoryArray[i].link,
+        comments: []
+    };
+
+    for (var j = 0; j < StoryArray[i].comments.length; j++) {
+        for (var k = 0; k < CommentArray.length; k++) {
+            if (StoryArray[i].comments[j] === CommentArray[k]._id) {
+                s.comments.push(CommentArray[k]);
+            }
+        }
+    }
+    resObj.push(s);
+    console.log('New Story')
+}
+console.log(resObj);
+
+// StoryArray.forEach(story) {
+//     story.comments.forEach(individual_comment) {
+//         CommentArray.forEach(comment) {
+//             if (individual_comment._id === comment._id) {
+//                 individual_comment = comment;
+//             }
+//         }
+//     }
+// }
+
+var strObj = JSON.stringify(resObj);
+console.log(strObj);
+process.exit();
--- router/routes/api-routes.js
+++ router/routes/api-routes.js
@@ -4,15 +4,49 @@ module.exports = function(app, db, approot) {
     var Comment = require(path.join(approot, '/models/Comment.js'));
     var Story = require(path.join(approot, '/models/Story.js'));
 
+    // Get the main page and populated it with stories from the database
     app.get("/", function(req, res) {
         console.log('app.get("/", function(req, res) {');
 
+        // Other parts were developed and implemented to receive the Story and 
+        // the Comment response in a specific form. The Story object would have
+        // the Comment object embedded in it. Below I get the Story array and
+        // then the Comment array and assemble respObj (response Object) to be 
+        // in this form.
         Story.find({}, function(error, storyArray) {
             if (error) {
                 console.log(error);
             } else {
-                res.json(storyArray);
-                //res.render('index', { stories: storyArray });
+                var respObj = []; // The response object.
+                Comment.find({}, function(error, commentArray) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        // 1.) For each story
+                        for (var i = 0; i < storyArray.length; i++) {
+                            // 2.) Create a corresponding story variable for the respObj
+                            var responseStory = {
+                                _id: storyArray[i]._id,
+                                headline: storyArray[i].headline,
+                                link: storyArray[i].link,
+                                comments: [] // will be populated with all the comments
+                            };
+                            // 3.) For each comment in each story
+                            for (var j = 0; j < storyArray[i].comments.length; j++) {
+                                // 4.) Go through all the comments in the comments array
+                                for (var k = 0; k < commentArray.length; k++) {
+                                    // 5.) When matching comment found
+                                    if (storyArray[i].comments[j].toString() === commentArray[k]._id.toString()) {
+                                        // 6.) Push comment into respObj responseStory's comments array
+                                        responseStory.comments.push(commentArray[k]);
+                                    }
+                                }
+                            }
+                            respObj.push(responseStory);
+                        }
+                        res.render('index', { stories: respObj });
+                    }
+                });
             }
         });
     });
@@ -24,10 +58,9 @@ module.exports = function(app, db, approot) {
         res.send('app.post("/", function(req, res) {');
     });
 
+    // Add new comments to database and then redirect to app.get('/') 
     app.put("/:id", function(req, res) {
         console.log('app.put("/:id", function(req, res) {');
-        console.log('req.params.id:' + req.params.id);
-        console.log('req.body.comment:' + req.body.comment);
         var comment = new Comment({ body: req.body.comment });
         comment.save(function(error, savedComment) {
             if (error) {
@@ -35,7 +68,6 @@ module.exports = function(app, db, approot) {
             } else {
                 console.log(savedComment);
                 commentId = savedComment._id;
-                // req.params.id
                 Story.findOneAndUpdate({ "_id": req.params.id }, { $push: { "comments": savedComment._id } }, { new: true }, function(err, newdoc) {
                     if (err) {
                         console.log(err);
